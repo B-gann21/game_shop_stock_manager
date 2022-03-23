ab0c20ff72ae78a74cebc9a2788bd7eb9328257f
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-23 16:02:11 -0600

    Refactor: test for GameShop#index re-written to be proper syntax

--- spec/features/game_shop_index_spec.rb
+++ spec/features/game_shop_index_spec.rb
@@ -1,17 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'GameShop index', type: :feature do
-  context '/game_shops' do
-    before :each do
-      # User Story 1, Parent Index
-      #
-      # For each parent table
-      # As a visitor
-      # When I visit '/parents'
-      # Then I see the name of each parent record in the system
+  describe '/game_shops' do
+    it 'displays all GameShop names' do
       shop1 = GameShop.create(name: "Fred's Games", overstocked: false, stock_limit: 3)
       shop2 = GameShop.create(name: "Gameville", overstocked: false, stock_limit: 3)
-
+      
       visit '/game_shops'
 
       expect(page).to have_content(shop1.name)
