c1b5643f7ea5c550cbb4da3257f63af701366f31
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-23 19:32:06 -0600

    Route & Controller made for /video_games

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/game_shops', to: 'game_shops#index'
   get '/game_shops/:id', to: 'game_shops#show'
+  get '/video_games', to: 'video_games#index'
 end
