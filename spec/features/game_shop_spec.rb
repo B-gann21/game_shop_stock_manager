0f6d533b03e34f6b4dcebf1efba11ab519cbf54f
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-23 20:27:10 -0600

    Refactor FeatureTest: game_shop_spec now uses 'before :each'

--- spec/features/game_shop_spec.rb
+++ spec/features/game_shop_spec.rb
@@ -1,35 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe 'GameShop', type: :feature do
-  describe '/game_shops' do
-    it 'displays all GameShop names' do
-      shop1 = GameShop.create!(name: "Fred's Games", overstocked: false, stock_limit: 3)
-      shop2 = GameShop.create!(name: "Gameville", overstocked: false, stock_limit: 3)
+  before :each do
+    @shop1 = GameShop.create!(name: "Fred's Games", overstocked: false, stock_limit: 3)
+    @shop2 = GameShop.create!(name: "Gameville", overstocked: false, stock_limit: 5)
+  end
 
+  context '/game_shops' do
+    it 'displays all GameShop names' do
       visit '/game_shops'
 
-      expect(page).to have_content(shop1.name)
-      expect(page).to have_content(shop2.name)
+      expect(page).to have_content(@shop1.name)
+      expect(page).to have_content(@shop2.name)
     end
   end
 
-  describe '/game_shops/:id' do
+  context '/game_shops/:id' do
     it 'displays the matching GameShop and attibutes' do
-      shop1 = GameShop.create!(name: "Fred's Games", overstocked: false, stock_limit: 3)
-      shop2 = GameShop.create!(name: "Gameville", overstocked: false, stock_limit: 5)
+      visit "/game_shops/#{@shop1.id}"
 
-      visit "/game_shops/#{shop1.id}"
+      expect(page).to have_content("ID: #{@shop1.id}")
+      expect(page).to have_content(@shop1.name)
+      expect(page).to have_content("Overstocked: #{@shop1.overstocked}")
+      expect(page).to have_content("Stock limit: #{@shop1.stock_limit}")
+      expect(page).to have_content("Created at: #{@shop1.created_at}")
+      expect(page).to have_content("Updated at: #{@shop1.updated_at}")
 
-      expect(page).to have_content("ID: #{shop1.id}")
-      expect(page).to have_content(shop1.name)
-      expect(page).to have_content("Overstocked: #{shop1.overstocked}")
-      expect(page).to have_content("Stock limit: #{shop1.stock_limit}")
-      expect(page).to have_content("Created at: #{shop1.created_at}")
-      expect(page).to have_content("Updated at: #{shop1.updated_at}")
-
-      expect(page).to_not have_content("ID: #{shop2.id}")
-      expect(page).to_not have_content(shop2.name)
-      expect(page).to_not have_content("Stock limit: #{shop2.stock_limit}")
+      expect(page).to_not have_content("ID: #{@shop2.id}")
+      expect(page).to_not have_content(@shop2.name)
+      expect(page).to_not have_content("Stock limit: #{@shop2.stock_limit}")
     end
+  end
+
+  context '/game_shops/:id/video_games' do
+    # As a visitor
+    # When I visit '/parents/:parent_id/child_table_name'
+    # Then I see each Child that is associated with that Parent with each Child's attributes:
   end
 end
