d027c84c79f3f2f3de46c34c9a7bb0c36796e6b6
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-23 19:40:48 -0600

    Feat: /video_games displays all video game names

--- spec/features/video_game_spec.rb
+++ spec/features/video_game_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'VideoGame' do
   describe '/video_games' do
     it 'displays all VideoGame names' do
       shop1 = GameShop.create!(name: "Fred's Games", overstocked: false, stock_limit: 3)
-      game1 = shop1.video_games.create!(name: "DOOM 2016", rating: "M", price: 60, multiplayer: false)
+      game1 = shop1.video_games.create!(name: "DOOM 2016", rating: "M", price: 70, multiplayer: false)
       game2 = shop1.video_games.create!(name: "FIFA 2020", rating: "E", price: 55, multiplayer: true)
       game3 = shop1.video_games.create!(name: "Elden Ring", rating: "M", price: 65, multiplayer: true)
 
