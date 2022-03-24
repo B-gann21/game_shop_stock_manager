7c575eb9ab4dbc2f1cb963ea031ef339f35eb698
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-23 19:27:52 -0600

    Refactor FeatureTest: /video_games displays all video games

--- spec/features/game_shop_spec.rb
+++ spec/features/game_shop_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe 'GameShop', type: :feature do
   describe '/game_shops' do
     it 'displays all GameShop names' do
-      shop1 = GameShop.create(name: "Fred's Games", overstocked: false, stock_limit: 3)
-      shop2 = GameShop.create(name: "Gameville", overstocked: false, stock_limit: 3)
+      shop1 = GameShop.create!(name: "Fred's Games", overstocked: false, stock_limit: 3)
+      shop2 = GameShop.create!(name: "Gameville", overstocked: false, stock_limit: 3)
 
       visit '/game_shops'
 
@@ -15,8 +15,8 @@ RSpec.describe 'GameShop', type: :feature do
 
   describe '/game_shops/:id' do
     it 'displays the matching GameShop and attibutes' do
-      shop1 = GameShop.create(name: "Fred's Games", overstocked: false, stock_limit: 3)
-      shop2 = GameShop.create(name: "Gameville", overstocked: false, stock_limit: 5)
+      shop1 = GameShop.create!(name: "Fred's Games", overstocked: false, stock_limit: 3)
+      shop2 = GameShop.create!(name: "Gameville", overstocked: false, stock_limit: 5)
 
       visit "/game_shops/#{shop1.id}"
 
@@ -26,7 +26,7 @@ RSpec.describe 'GameShop', type: :feature do
       expect(page).to have_content("Stock limit: #{shop1.stock_limit}")
       expect(page).to have_content("Created at: #{shop1.created_at}")
       expect(page).to have_content("Updated at: #{shop1.updated_at}")
-      sleep 1
+
       expect(page).to_not have_content("ID: #{shop2.id}")
       expect(page).to_not have_content("Name: #{shop2.name}")
       expect(page).to_not have_content("Stock limit: #{shop2.stock_limit}")
