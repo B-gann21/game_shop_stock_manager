3a3e4204210f2449ce0116ded7acd3b42c6530a1
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-24 16:57:08 -0600

    Feat: GameShops#index orders entries by created_date

--- spec/features/game_shop/index_spec.rb
+++ spec/features/game_shop/index_spec.rb
@@ -9,14 +9,31 @@ RSpec.describe 'GameShops', type: :feature do
       @shop3 = GameShop.create!(name: "GoodGames", does_repairs: false, stock_limit: 5)
     end
 
-    it 'displays all GameShop names' do
+    it 'displays all names' do
       visit '/game_shops'
 
       expect(page).to have_content(@shop1.name)
       expect(page).to have_content(@shop2.name)
     end
 
-    it 'displays all GameShop created_at attributes next to the name' do
+    it 'displays in order of created_at attribute' do
+      visit '/game_shops'
+      content = page.text
+      shop1_position = content.index("#{@shop1.name}")
+      shop2_position = content.index("#{@shop2.name}")
+      shop3_position = content.index("#{@shop3.name}")
+
+      expect(shop1_position).to be < shop2_position
+      expect(shop1_position).to be < shop3_position
+
+      expect(shop2_position).to be < shop3_position
+      expect(shop2_position).to be > shop1_position
+
+      expect(shop3_position).to be > shop1_position
+      expect(shop3_position).to be > shop2_position
+    end
+
+    it 'displays created_at attributes next to the names' do
       visit '/game_shops'
 
       expect(page).to have_content("#{@shop1.name} -- Created: #{@shop1.created_at}")
