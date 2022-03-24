ad665d283031a2974b40468d3266293cdfa2f68c
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-24 14:07:07 -0600

    Model: GameShop validates presence of :name, :does_repairs, :stock_limit

--- app/models/game_shop.rb
+++ app/models/game_shop.rb
@@ -1,3 +1,4 @@
 class GameShop < ApplicationRecord
   has_many :video_games
+  validates_presence_of :name, :does_repairs, :stock_limit
 end
--- spec/models/game_shop/validation_spec.rb
+++ spec/models/game_shop/validation_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe GameShop, type: :model do
   describe "validation" do
-    it { should validate_presence_of :name, :does_repairs, :stock_limit }
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :does_repairs }
+    it { should validate_presence_of :stock_limit }
   end
 end
