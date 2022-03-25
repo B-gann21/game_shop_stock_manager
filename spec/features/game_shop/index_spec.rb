86bc0c7f99003e3bfa34d7c56e55222342893fba
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-24 22:22:21 -0600

    Feat: GameShops#index shows link to VideoGames#index

--- spec/features/game_shop/index_spec.rb
+++ spec/features/game_shop/index_spec.rb
@@ -7,12 +7,21 @@ RSpec.describe 'GameShops', type: :feature do
       @shop1 = GameShop.create!(name: "Fred's Games", does_repairs: false, stock_limit: 3)
       @shop2 = GameShop.create!(name: "Gameville", does_repairs: false, stock_limit: 5)
       @shop3 = GameShop.create!(name: "GoodGames", does_repairs: false, stock_limit: 5)
+      @game1 = @shop1.video_games.create!(name: "DOOM 2016", rating: "M", price: 70, multiplayer: false)
+      @game2 = @shop1.video_games.create!(name: "FIFA 2020", rating: "E", price: 55, multiplayer: true)
+      @game3 = @shop2.video_games.create!(name: "Elden Ring", rating: "M", price: 65, multiplayer: true)
 
       visit '/game_shops'
     end
 
     it 'displays a link to the video_game_index' do
       expect(page).to have_link('Video Game Index')
+
+      click_link('Video Game Index')
+
+      expect(page).to have_content(@game1.name)
+      expect(page).to have_content(@game2.name)
+      expect(page).to have_content(@game3.name)
     end
 
     it 'displays all names' do
