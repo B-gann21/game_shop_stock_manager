1e6d342b5adb6a31c7719d40248cd847c355be39
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-26 12:49:39 -0600

    Refactortest: remove /game_shop/links directory, move link tests to spec files

--- spec/features/game_shop/show_spec.rb
+++ spec/features/game_shop/show_spec.rb
@@ -27,4 +27,42 @@ RSpec.describe 'GameShops' do
       expect(page).to_not have_content("Stock limit: #{@shop2.stock_limit}")
     end
   end
+
+  context 'index links' do
+    it 'displays a link to the VideoGame index' do
+      expect(page).to have_link('Video Game Index')
+
+      click_link('Video Game Index')
+
+      expect(page).to have_content(@game1.name)
+      expect(page).to have_content(@game2.name)
+      expect(page).to have_content(@game3.name)
+    end
+
+    it 'displays a link to the GameShop index' do
+      expect(page).to have_link('Game Shop Index')
+
+      click_link('Game Shop Index')
+
+      expect(page).to have_content(@shop1.name)
+      expect(page).to have_content(@shop2.name)
+    end
+
+    it 'displays a link for all related games' do
+      expect(page).to have_link("Games sold by #{@shop1.name}")
+
+      click_link("Games sold by #{@shop1.name}")
+
+      expect(page).to have_content(@game1.name)
+      expect(page).to have_content(@game2.name)
+      expect(page).to_not have_content(@game3.name)
+    end
+
+    it 'should display index links at the top of the page' do
+      expect("Video Game Index").to appear_before("Game Shop Index")
+      expect("Video Game Index").to appear_before(@shop1.name)
+
+      expect("Game Shop Index").to appear_before(@shop1.name)
+    end
+  end
 end
