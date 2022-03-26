50dbd685a56da9b7cf316406aa21cde874dbab9f
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-26 12:43:15 -0600

    Remove /shop_games/links directory, move link tests to spec file

--- spec/features/shop_games/index_spec.rb
+++ spec/features/shop_games/index_spec.rb
@@ -42,4 +42,51 @@ RSpec.describe 'ShopGames' do
       end
     end
   end
+
+  context 'index links' do
+    it 'displays a link to the VideoGame index' do
+      expect(page).to have_link('Video Game Index')
+
+      click_link('Video Game Index')
+
+      expect(page).to have_content(@game1.name)
+      expect(page).to have_content(@game2.name)
+      expect(page).to have_content(@game3.name)
+    end
+
+    it 'displays a link to the GameShop index' do
+      expect(page).to have_link('Game Shop Index')
+
+      click_link('Game Shop Index')
+
+      expect(page).to have_content(@shop1.name)
+      expect(page).to have_content(@shop2.name)
+    end
+
+    it 'should display links at the top of the page' do
+      expect("Video Game Index").to appear_before("Game Shop Index")
+      expect("Video Game Index").to appear_before("Games from shop #")
+
+      expect("Game Shop Index").to appear_before("Games from shop #")
+    end
+  end
+
+  context 'show links' do
+    it "has each game's name as a link" do
+      expect(page).to have_link("DOOM 2016")
+      expect(page).to have_link("FIFA 2020")
+    end
+
+    it "clicking DOOM 2016 brings you to it's show page" do
+      click_link('DOOM 2016')
+
+      expect(current_path).to eq("/video_games/#{@game1.id}")
+    end
+
+    it "clicking on FIFA 2020 takes you to it's show page" do
+      click_link('FIFA 2020')
+
+      expect(current_path).to eq("/video_games/#{@game2.id}")
+    end
+  end
 end
