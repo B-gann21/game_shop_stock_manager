d95185b7fdd839d399091719865c37ffd7e22fd9
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-27 11:31:02 -0600

    Remove user story from spec files

--- spec/features/game_shop/edit_spec.rb
+++ spec/features/game_shop/edit_spec.rb
@@ -5,16 +5,7 @@ RSpec.describe "GameShop#edit" do
     @shop1 = GameShop.create!(name: "Fred's Games", does_repairs: false, stock_limit: 3)
     visit "/game_shops/#{@shop1.id}"
   end
-  # As a visitor
-  # When I visit a parent show page
-  # Then I see a link to update the parent "Update Parent"
-  # When I click the link "Update Parent"
-  # Then I am taken to '/parents/:id/edit' where I  see a form to edit the parent's attributes:
-  # When I fill out the form with updated information
-  # And I click the button to submit the form
-  # Then a `PATCH` request is sent to '/parents/:id',
-  # the parent's info is updated,
-  # and I am redirected to the Parent's Show page where I see the parent's updated info
+
   context 'Edit link functionality' do
     it 'should have a link to edit the Game Shop' do
       expect(page).to have_link("Edit Fred's Games")
--- spec/features/game_shop/new_spec.rb
+++ spec/features/game_shop/new_spec.rb
@@ -11,16 +11,6 @@ RSpec.describe 'GameShop#new' do
 
     visit '/game_shops'
   end
-  # As a visitor
-  # When I visit the Parent Index page
-  # Then I see a link to create a new Parent record, "New Parent"
-  # When I click this link
-  # Then I am taken to '/parents/new' where I  see a form for a new parent record
-  # When I fill out the form with a new parent's attributes:
-  # And I click the button "Create Parent" to submit the form
-  # Then a `POST` request is sent to the '/parents' route,
-  # a new parent record is created,
-  # and I am redirected to the Parent Index page where I see the new Parent displayed.
 
   context 'New Game Shop link functionality' do
     it 'should have a link called New Game Shop' do
