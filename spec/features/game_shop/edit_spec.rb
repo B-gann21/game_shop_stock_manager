c998e44998827333466fa4790a4e8d95e6ddebd5
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-27 11:14:01 -0600

    FeatTest: GameShops#edit page should have a form to change attributes

--- spec/features/game_shop/edit_spec.rb
+++ spec/features/game_shop/edit_spec.rb
@@ -26,4 +26,18 @@ RSpec.describe "GameShop#edit" do
       expect(current_path).to eq("/game_shops/#{@shop1.id}/edit")
     end
   end
+
+  context 'editing a Game Shop' do
+    it 'after submitting the form, you can see the new attributes' do
+      click_link "Edit Fred's Games"
+
+      fill_in :name, with: "Fred's Game Shop"
+      fill_in :does_repairs, with: "true"
+      click_button "Update Game Shop"
+
+      expect(current_path).to eq("/game_shops/#{@shop1.id}")
+      expect(page).to have_content("Fred's Game Shop")
+      expect(page).to have_content("Does repairs: true")
+    end
+  end
 end
