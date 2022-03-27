53bcb242f0753536bef0bc9f1b9bb3e35f36b00c
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-27 12:14:03 -0600

    Feat: submitting the form created a new game

--- app/controllers/shop_games_controller.rb
+++ app/controllers/shop_games_controller.rb
@@ -5,5 +5,17 @@ class ShopGamesController < ApplicationController
   end
 
   def new
+    @game_shop = GameShop.find(params[:id])
+  end
+
+  def create
+    game_shop = GameShop.find(params[:id])
+    game_shop.video_games.create(video_game_params)
+    redirect_to "/game_shops/#{game_shop.id}/video_games"
+  end
+
+private
+  def video_game_params
+    params.permit :name, :rating, :price, :multiplayer
   end
 end
