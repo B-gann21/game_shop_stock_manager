0f1cff4d349e9dddc9a273762d6db40efacbd3cd
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-30 15:40:28 -0600

    Feat: Welcome page now has the title of the app and index links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'welcome#index'
+
   get '/game_shops', to: 'game_shops#index'
   get '/game_shops/new', to: 'game_shops#new'
   post '/game_shops', to: 'game_shops#create'
